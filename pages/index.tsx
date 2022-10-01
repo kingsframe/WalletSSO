5ba71ab070c9d3af9f5d039a5170111a73701936
kaiwen zhang <kaiwen.zhang@carta.com>
2022-10-01 13:24:52 -0700

    login successful

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,65 @@
+import { initializeApp } from "firebase/app";
+import {
+  Auth,
+  connectAuthEmulator,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import type { NextPage } from "next";
-import { useContext } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
-import { FirebaseContext } from "./_app";
+import { firebaseConfig } from "../utils/FirebaseConfig";
 
 const Home: NextPage = () => {
-  const { auth } = useContext(FirebaseContext);
+  const [auth, setAuth] = useState<Auth | null>(null);
 
-  console.log("auth: ", auth);
+  useEffect(() => {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const firebaseAuth = getAuth(app);
+    setAuth(firebaseAuth);
+    connectAuthEmulator(firebaseAuth, "http://localhost:9099");
+  }, []);
+
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginEmailPassword = async () => {
+    if (!auth) return console.error("No auth provided yet!");
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log("userCredential: ", userCredential);
+    } catch (error) {
+      console.log(`There was an error: ${error}`);
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <form action="">
-        <div>
-          <label>Enter your username: </label>
-          <input type="text" name="name" id="name" required />
-        </div>
-        <div>
-          <label>Enter your password: </label>
-          <input type="text" name="email" id="email" required />
-        </div>
-        <div>
-          <input type="submit" value="Log in" />
-        </div>
-      </form>
+      <div>
+        <label>Enter your email: </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={(e) => setemail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Enter your password: </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={loginEmailPassword}>Log In</button>
     </div>
   );
 };
