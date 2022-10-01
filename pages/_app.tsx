d85fac917ebe72ab0aaa7b8c4545002542cb5c35
kaiwen zhang <kaiwen.zhang@carta.com>
2022-10-01 12:48:16 -0700

    WIP firebase context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,46 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Analytics, getAnalytics } from "firebase/analytics";
+import { Auth, getAuth } from "firebase/auth";
+import { createContext } from "react";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5Nx4OqLsnHLvUxkAHOxlsaQeMs_XQ7CU",
+  authDomain: "walletsso.firebaseapp.com",
+  projectId: "walletsso",
+  storageBucket: "walletsso.appspot.com",
+  messagingSenderId: "937373074013",
+  appId: "1:937373074013:web:d2eafd29bbfc1fa76843b4",
+  measurementId: "G-G9SQKTCG3R",
+};
+
+type FirebaseContextT = {
+  app: FirebaseApp;
+  analytics: Analytics;
+  auth: Auth;
+};
+
+export const FirebaseContext = createContext<FirebaseContextT | null>(null);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth = getAuth(app);
+
+  return (
+    <FirebaseContext.Provider
+      value={{
+        app,
+        analytics,
+        auth,
+      }}
+    >
+      <Component {...pageProps} />
+    </FirebaseContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
